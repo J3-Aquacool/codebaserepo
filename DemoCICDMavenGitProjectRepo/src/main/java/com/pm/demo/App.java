51cd70c679dadf3d01f401b67227e240d88c75b7
Jagadish Hegade <42802228+J3-Aquacool@users.noreply.github.com>
2025-04-10 17:22:20 +0530

    Update App.java

--- DemoCICDMavenGitProjectRepo/src/main/java/com/pm/demo/App.java
+++ DemoCICDMavenGitProjectRepo/src/main/java/com/pm/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Maven + Git +Jenkins" );
+        System.out.println( "Maven + Git +Jenkins lets update" );
     }
 }
