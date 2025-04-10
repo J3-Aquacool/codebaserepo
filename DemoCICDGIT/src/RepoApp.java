c7550064e0b868ce1b8cdfad8b092c43a5d942a9
Jagadish Hegade <42802228+J3-Aquacool@users.noreply.github.com>
2025-04-10 12:54:12 +0530

    Added the way we work

--- DemoCICDGIT/src/RepoApp.java
+++ DemoCICDGIT/src/RepoApp.java
@@ -4,7 +4,7 @@ public class RepoApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("I will be managed by Git repo gain");
+		System.out.println("I will be managed by Git repo gain lets chnage the way we work");
 	}
 
 }
